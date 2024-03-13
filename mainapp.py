3c4d4a6dcc56474c1eee56b9c20c6572d5e3211b
coffiholic <108257436+sucheerarighthere@users.noreply.github.com>
2024-03-13 12:31:18 +0700

    Update mainapp.py

--- mainapp.py
+++ mainapp.py
@@ -111,58 +111,6 @@
 #         except Exception as e:
 #             st.error(f"Error processing file: {e}")
 # ================================
-# import argparse
-# import streamlit as st
-# import io
-# from PIL import Image
-# import numpy as np
-# import torch
-# import cv2
-# import detect
-# # Set image
-# image = Image.open('STAT-Header-Logo-V7.png')
-# st.image(image, caption='สาขาวิชาสถิติ คณะวิทยาศาสตร์ มหาวิทยาลัยขอนแก่น', use_column_width=True)
-
-# # Load YOLOv5 model
-# model = torch.hub.load('ultralytics/yolov5', 'custom', path='models/bestyolo.pt')
-
-# # Use st.file_uploader for file upload
-# uploaded_files = st.file_uploader("Choose .jpg pic ...", type="jpg", accept_multiple_files=True)
-
-# # Loop through uploaded files
-# for uploaded_file in uploaded_files:
-#     if uploaded_file is not None:
-#         try:
-#             # Read and decode the uploaded image
-#             file_bytes = np.asarray(bytearray(uploaded_file.read()))
-#             image = cv2.imdecode(file_bytes, 1)
-#             imgRGB = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-
-#             # Display information about the detection process
-#             st.write("")
-#             st.write("Detecting...")
-
-#             # Run YOLOv5 model on the image
-#             result = model(imgRGB, size=300)
-
-#             # Extract detected objects' information
-#             detect_class = result.pandas().xyxy[0]
-
-#             # Display the original image
-#             st.image(imgRGB, caption='Original Image', use_column_width=True)
-
-#             # Display bounding boxes without class names and confidence scores
-#             for pred in detect_class:
-#                 bbox = pred[:4]
-#                 st.image(cv2.rectangle(imgRGB.copy(), tuple(bbox[:2]), tuple(bbox[2:]), (0, 255, 0), 2), use_column_width=True)
-
-#             # Display the number of detected objects
-#             num_objects_detected = len(detect_class)
-#             st.write(f"Number of objects detected: {num_objects_detected}")
-
-#         except Exception as e:
-#             # Display an error message if an exception occurs during processing
-#             st.error(f"Error processing file: {e}")
 import argparse
 import streamlit as st
 import io
@@ -181,6 +129,7 @@ model = torch.hub.load('ultralytics/yolov5', 'custom', path='models/bestyolo.pt'
 # Use st.file_uploader for file upload
 uploaded_files = st.file_uploader("Choose .jpg pic ...", type="jpg", accept_multiple_files=True)
 
+# Loop through uploaded files
 for uploaded_file in uploaded_files:
     if uploaded_file is not None:
         try:
@@ -211,11 +160,7 @@ for uploaded_file in uploaded_files:
             num_objects_detected = len(detect_class)
             st.write(f"Number of objects detected: {num_objects_detected}")
 
-            # Break the loop to process only one uploaded image
-            break
-
         except Exception as e:
             # Display an error message if an exception occurs during processing
             st.error(f"Error processing file: {e}")
-
 
