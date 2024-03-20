2bd21792216305564f1fb29ed1a2f3d2398af192
coffiholic <108257436+sucheerarighthere@users.noreply.github.com>
2024-03-20 16:48:20 +0700

    Update 3_app.py

--- 3_app.py
+++ 3_app.py
@@ -92,9 +92,10 @@ if uploaded_files:
                         # ax.text(xmin, ymin,row['name'], color='r')  # Add the name of the object on the bounding box
 
                     # Show the image with bounding boxes
-                    # col2.pyplot(fig)
-                    col2.write(f"<h1 style='text-align: center;'>Number of  detected chromosomes: {num_objects_detected}<br></h1>", unsafe_allow_html=True)
                     col2.pyplot(fig)
+                    col2.write(f"<h1 style='text-align: center;'>Number of  detected chromosomes: {num_objects_detected}<br></h1>", unsafe_allow_html=True)
+                    # col2.pyplot(fig)
+                    col2.write("=" * 80)
 
             except Exception as e:
                 st.write(f"Error: {e}") 
